9ab62111eff0fe5d0065a9db5d81deb4cde23f7f
adadarsh23 <adarshkumr004@gmail.com>
2025-10-04 01:56:24 +0530

    add the new features

--- Data/game1.js
+++ Data/game1.js
@@ -2,49 +2,33 @@ const games1 = [
   {
     id: "google-play",
     platform: "Google Play Games",
-    profileName: "Âd Adarsh",
-    profileId: "2656842246",
-    url: "https://play.google.com/store/apps/details?id=com.example.game",
-    image: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Play_Games_icon_%282022%29.svg"
+    profileName: "ÂdAdarsh",
+    profileId: "adadarsh23",
+    url: "https://games.app.goo.gl/mApBVvgLaC7F6Ksx8",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/95/Google_Play_Games_logo_%282023%29.svg"
   },
   {
     id: "xbox",
-    platform: "Xbox Live",
-    profileName: "AdAdarsh23",
-    profileId: "XBL-1234567890",
+    platform: "Xbox",
+    profileName: "adAdarsh23",
+    profileId: "adadarsh23",
     url: "https://account.xbox.com/en-us/Profile?gamerTag=AdAdarsh23",
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox_one_logo.svg"
-  },
-  {
-    id: "playstation",
-    platform: "PlayStation Network",
-    profileName: "Adarsh_PS",
-    profileId: "PSN-987654321",
-    url: "https://my.playstation.com/profile/Adarsh_PS",
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/Xbox_app_logo.svg"
   },
   {
     id: "steam",
     platform: "Steam",
-    profileName: "AdarshSteam",
-    profileId: "STEAM_0:1:12345678",
-    url: "https://steamcommunity.com/id/AdarshSteam",
-    image: "https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"
-  },
-  {
-    id: "riot",
-    platform: "Riot Games",
     profileName: "Âd Adarsh",
-    profileId: "GAME NAME#TAGLINE",
-    url: "https://www.riotgames.com/",
-    image: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Riot_Games_wordmark.svg"
+    profileId: "adadarsh23",
+    url: "https://steamcommunity.com/id/adadarsh23",
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"
   },
   {
     id: "epic-games",
     platform: "Epic Games",
-    profileName: "AdarshEpic",
-    profileId: "EPIC-123456789",
-    url: "https://store.epicgames.com/en-US/u/AdarshEpic",
+    profileName: "Âd Adarsh",
+    profileId: "98722d9b2fb44ba68d707eeb18a67a14",
+    url: "https://store.epicgames.com/u/98722d9b2fb44ba68d707eeb18a67a14",
     image: "https://upload.wikimedia.org/wikipedia/commons/3/31/Epic_Games_logo.svg"
   }
 ];
